d9a4b46e99d97cf40236b654e9bc89644c6e7939
Masoud Roosta <masdevoud@gmail.com>
2022-10-27 11:11:35 +0200

    Using Combine for building network layer unit

--- ADASTRA/ADASTRA/Utilities/Networking/Networking.swift
+++ ADASTRA/ADASTRA/Utilities/Networking/Networking.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import Combine
+
+enum HTTPRequestMethod: String{
+    case get               =               "GET"
+    case post              =               "POST"
+    case put               =               "PUT"
+    case delete            =               "DELETE"
+}
+
